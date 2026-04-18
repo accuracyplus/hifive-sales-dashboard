2f59e0ee3d1a9ec38c287774dce45fed13221b25
accuracyplus <sinusuresh@gmail.com>
2026-04-18 15:39:40 +0400

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const HIFIVE_CONFIG = {
+  CLIENT_ID: '829950296489-rdaoraktfujf4fkgkpn6ag79dsdml18j.apps.googleusercontent.com',
+  API_URL:   'https://script.google.com/macros/s/AKfycbxZ-WJxZRjelokix-1XWp2WHJgMs-CIHx3n4tLrgMCdQDPksPUqq8keAYqRLfROSCKf5g/exec',
+};
